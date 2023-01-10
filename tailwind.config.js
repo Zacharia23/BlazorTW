259ead4e228c0bde86916f0e2e24b93046b903c7
Zacharia23 <zachariabaraka8@gmail.com>
2023-01-10 12:38:08 +0300

    (initial) configurations and sample reference project

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["**/*.razor", "**/*.cshtml", "**/*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
